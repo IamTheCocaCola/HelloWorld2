75df4a162f9c3f8ee52720284e7d39d02e2ef52d
Sawyer Mallonee <temple77@comcast.net>
2023-01-11 10:40:36 -0700

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Mallonee, Sawyer - Student on 1/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
